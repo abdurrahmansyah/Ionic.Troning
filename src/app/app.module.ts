5474ee6f2d8d1ac78e2845cb0dd05db2852d3541
Nur Ridho A <abdurrahmansyah234@gmail.com>
2022-08-25 10:39:16 +0700

    setting firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,37 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth, initializeAuth, indexedDBLocalPersistence } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, enableIndexedDbPersistence } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { Capacitor } from '@capacitor/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    provideFirebaseApp(() => {
+      const app = initializeApp(environment.firebase);
+      if (Capacitor.isNativePlatform) {
+        initializeAuth(app, {
+          persistence: indexedDBLocalPersistence
+        });
+      }
+      return app;
+    }),
+    provideAuth(() => getAuth()), 
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      enableIndexedDbPersistence(firestore);
+      return firestore;
+    }),
+    provideFunctions(() => getFunctions())
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'truckmonitoring-fa8b7',
+    appId: '1:614624906743:web:60203349696dae303e32f2',
+    storageBucket: 'truckmonitoring-fa8b7.appspot.com',
+    locationId: 'asia-northeast2',
+    apiKey: 'AIzaSyBMeqFgYnSSKVIn59LellLSPDfcqkcBzfM',
+    authDomain: 'truckmonitoring-fa8b7.firebaseapp.com',
+    messagingSenderId: '614624906743',
+    measurementId: 'G-8NXC69JMDC',
+  },
   production: true
 };
